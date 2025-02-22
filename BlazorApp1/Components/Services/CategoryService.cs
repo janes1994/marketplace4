f0da8bebab98adb3feb3e85450825edbc6b844a5
janes1994 <yanesreksandi@yaness-MacBook-Air.local>
2025-02-22 07:36:28 +0700

    move hardcode category to category service

--- BlazorApp1/Components/Services/CategoryService.cs
+++ BlazorApp1/Components/Services/CategoryService.cs
@@ -7,9 +7,47 @@ namespace BlazorApp1.Components.Services;
 
 public class CategoryService
 {
-    public List<FirstCategoryView> FirstCategory { get; private set; } = [];
-    public List<SecondCategoryView> SecondCategory { get; private set; } = [];
-    public List<ThirdCategoryView> ThirdCategory { get; private set; } = [];
+    public List<FirstCategoryView> FirstCategory { get; private set; } =
+    [
+        new() { Id = "1", Name = "Computer", Icon = "Computer Icon", Banner = "Computer Banner"},   
+        new() { Id = "2", Name = "Phone", Icon = "Phone Icon", Banner = "Phone Banner"}
+    ];
+    public List<SecondCategoryView> SecondCategory { get; private set; } =
+    [
+        new() { Id = "1", Name = "Desktop", FirstCategory = new FirstCategoryView { Id = "1", Name = "Computer", Banner = "banner", Icon = "icon" }, Icon = "Computer Icon", Banner = "Computer Banner"},
+        new() { Id = "2", Name = "Laptop", FirstCategory = new FirstCategoryView { Id = "1", Name = "Computer", Banner = "banner", Icon = "icon" }, Icon = "Phone Icon", Banner = "Phone Banner"}
+    ];
+    public List<ThirdCategoryView> ThirdCategory { get; private set; } =
+    [
+        new() 
+        { 
+            Id = "1", 
+            Name = "Lenovo",
+            Banner = "Banner",
+            Icon = "Icon", 
+            FirstCategory = new FirstCategoryView 
+            {
+                Id = "1", 
+                Name = "Computer", 
+                Banner = "banner", 
+                Icon = "Icon"
+            },
+            SecondCategory = new SecondCategoryView
+            {
+                Id = "1",
+                Name = "Desktop",
+                Banner = "Banner",
+                Icon = "Icon",
+                FirstCategory = new FirstCategoryView()
+                {
+                    Id = "1", 
+                    Name = "Computer", 
+                    Banner = "banner", 
+                    Icon = "Icon"
+                }
+            } 
+        }   
+    ];
 
     public event Action? OnFirstCategoryChanged;
     public event Action? OnSecondCategoryChanged;
